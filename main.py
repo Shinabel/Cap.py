56270527364a31884bbc281497ee620f4df5ca77
Shinabel <abelshin0523@gmail.com>
2018-02-10 05:18:47 -0500

    Update main.py
    it is working woohoo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-
+import io
+from google.cloud import vision
 from flask import Flask, request, render_template, send_from_directory
 
 app = Flask(__name__)
@@ -13,7 +14,7 @@ def index():
 
 @app.route("/upload", methods=['POST'])
 def upload():
-    target = os.path.join(APP_ROOT, 'images')
+    target = os.path.join(APP_ROOT, 'images/')
     print(target)
 
     if not os.path.isdir(target):
@@ -28,4 +29,14 @@ def upload():
 
     return render_template("caption.html")
 
+vision_client = vision.Client()
+file_name = filename
 
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+    image = vision_client.image(
+        content=content, )
+
+labels = image.detect_labels()
+for label in labels:
+    print(label.description)
