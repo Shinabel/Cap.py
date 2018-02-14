80a9a5c6b8cafa2503da89076b261bf6b6f86cd0
Abel Shin <shin.ab@husky.neu.edu>
2018-02-14 01:48:02 -0500

    added documentation and fixed minor error

--- main.py
+++ main.py
@@ -22,12 +22,20 @@ api_key = 'dc6zaTOxFJmzC'
 
 @app.route("/")
 def index():
+    """
+    first page users will see.
+    """
     return render_template("index.html")
 
 @app.route("/loading", methods=['POST'])
 def loading():
+    """
+    creates a page loading page that shows a GIFs relevant to the
+    inserted image.
+    """
     target = os.path.join(APP_ROOT, 'static')
     print(target)
+
     filename = ""
     destination = ""
     if not os.path.isdir(target):
@@ -36,6 +44,7 @@ def loading():
     if not request.files.get('file', None):
         return 'No selected file'
 
+    #load the file
     file = request.files.getlist("file")[0]
     filename = secure_filename(file.filename)
     destination = "/".join([target, filename])
@@ -45,24 +54,32 @@ def loading():
     stringLabels = ""
     for label in labels:
         stringLabels += label.description + ", "
-    #######
+
+    #modify the embeded url acquired for better fit in the webpage.
     memeLink = memeSearch(labels[0].description) + "/200.gif"
     memeLink = memeLink.replace("embed", "media")
     memeLink = memeLink.replace("https://", "https://media2.")
-    print (memeLink)
-    ######
+    #print (memeLink)
+
     return render_template("loading.html", gif_link = memeLink, labels = stringLabels, image_destination = destination)
 
 @app.route("/upload", methods=['POST'])
 def upload():
+    """
+    Creates a page with a picture inserted, quotes generated,
+    and the key words with hashtag in front of them.
+    """
     destination = request.form['destination']
     stringlabels = request.form['labels']
     labels = stringlabels.split(", ")
     labels = labels[:-1]
     quotes = []
     hashtags = ""
+
     for label in labels:
-        quotes.extend(getQuotes(label)[:3])
+        #pick 7 quotes from the brainyquote.com.
+        quotes.extend(getQuotes(label)[:7])
+        # add "#" in front of the key word that were generated from google cloud vision.1
         hashtags += "#" + label.replace(" ", '') + " "
 
     encoded_string = ""
@@ -72,6 +89,10 @@ def upload():
     return render_template("caption.html", quotes = quotes, image = encoded_string, hashtags = hashtags)
 
 def googlecloud(destination):
+    """
+    Use the google cloud vision to acquire key words that are relevant
+    from the inserted image.
+    """
     vision_client = vision.Client()
     file_name = destination
 
@@ -81,24 +102,35 @@ def googlecloud(destination):
         content=content, )
 
     labels = image.detect_labels()
-    for label in labels:
-        print(label.description)
+
+    #for label in labels:
+    #   print(label.description)
     return labels
 
 def getQuotes(keyword):
+    """
+    Acquire quotes that are relevant to the keyword inserted
+    by web scarping (using BeautifulSoup) brainyquote.com
+    """
     quoteArray = []
 
+    #url used
     base_url = "http://www.brainyquote.com/quotes/keywords/"
     url = base_url + keyword + ".html"
     response_data = requests.get(url).text[:]
     soup = BeautifulSoup(response_data, 'html.parser')
     
+    # loop through the html source code of the website and find specific keys
     for item in soup.find_all("a", class_="b-qt"):
         quoteArray.append(item.get_text().rstrip())
 
     return quoteArray
 
 def memeSearch(text):
+    """
+    Using GIPHY API, generate a GIFs that are related to the
+    inputted text (key word).
+    """
     key = text
     limit = 1
 
